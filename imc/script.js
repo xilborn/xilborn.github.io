29720f84e4fa5433906821f3c54de95ecdbd7b8f
Esmyx <valentin.sicotpoulain@gmail.com>
2024-01-06 11:17:19 +0100

    Added IMC calculator
    + imc/
    + imc/index.html
    + imc/styles.css
    + imc/script.js

---
+++ imc/script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM loaded!');
+})
+
+const calc = document.getElementById('btn');        // DO
+const result = document.getElementById('result');
+const home = document.getElementById('home');       // DO
+const imc = document.getElementById('imc');         // DO
+const status = document.getElementById('status');
+
+home.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.location.href = "/";
+})
+
+calc.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const h = document.getElementById("height").value;
+    const w = document.getElementById("weight").value;
+
+    console.log("Height : " + h + " Weight : " + w);
+
+    if (h && w) {
+        imc.innerText = `Your IMC : ${w / (h * h)}`;
+        status.innerText = `Status : ${w / (h * h) < 18.5 ? "Underweight" : w / (h * h) < 24.9 ? "Normal" : w / (h * h) < 29.9 ? "Overweight" : "Obese"}`
+    } else {
+        imc.innerText = "Please enter values";
+    }
+})
